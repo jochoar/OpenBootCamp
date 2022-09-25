c4ed9bf51c24881843217287a9bdecb9f96c7045
Jonathan Ochoa <j.ochoar26@gmail.com>
2022-09-25 08:40:35 -0500

    add the (reduce) for sum de odd numbers

--- Pythonworks/filterOddnumbers.py
+++ Pythonworks/filterOddnumbers.py
@@ -1,5 +1,7 @@
 #In this second exercise, you have to create an application that will obtain the odd elements of a list 
 # passed by parameter with filter and will make a sum of all these elements obtained by means of reduce.
+from functools import reduce
+import re 
 
 def filterOdd(numbers):
     odd = []
@@ -13,4 +15,11 @@ def filterOdd(numbers):
 listNumbers = list(range(1,100))
 print(type(listNumbers))
 results = filterOdd(listNumbers)
-print(results)            
+print("The odd numbers: ", results)  
+
+def add(a, b):
+    return a + b
+
+print("The Sum of odd numbers is: ", reduce(add, results)) 
+ 
+     
