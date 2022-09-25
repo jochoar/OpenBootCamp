22c6425e39b92a286eab917100d444222784a7ac
Jonathan Ochoa <j.ochoar26@gmail.com>
2022-09-25 15:50:22 -0500

    Crating a window using tkinter

---
+++ Pythonworks/tkwindow.py
@@ -0,0 +1,33 @@
+
+import tkinter as tk
+
+class Aplication:
+   
+    def __init__(self):
+        self.window1=tk.Tk()
+        self.select=tk.IntVar()
+        self.select.set(None)
+        self.radio1=tk.Radiobutton(self.window1,text="Student ", variable=self.select, value=1)
+        self.radio1.grid(column=0, row=0)
+        self.radio2=tk.Radiobutton(self.window1,text="Teacher ", variable=self.select, value=2)
+        self.radio2.grid(column=0, row=1)
+        self.button1=tk.Button(self.window1, text="Show selected", command=self.showselected)
+        self.button1.grid(column=0, row=2)
+        self.button2=tk.Button(self.window1, text="Reset", command=self.resetFields)
+        self.button2.grid(column=0, row=3)
+        self.label1=tk.Label(self.window1,text="Option selected ")
+        self.label1.grid(column=0, row=4)
+        self.window1.mainloop()   
+         
+    def showselected(self):
+        if self.select.get()==1:
+            self.label1.configure(text="Option selected=Student")
+        if self.select.get()==2:
+            self.label1.configure(text="Option selected=Teacher")
+            
+    def resetFields(self):
+          self.select.set(None)
+          self.label1.config(text= f"")
+        
+
+aplication1=Aplication() 
