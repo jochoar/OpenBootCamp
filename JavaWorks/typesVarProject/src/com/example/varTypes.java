739463e5a7959ea7d5a5b9906752bbada4e6f1f7
Jonathan Ochoa <j.ochoar26@gmail.com>
2022-09-28 19:55:43 -0500

    Start the Basic Java

---
+++ JavaWorks/typesVarProject/src/com/example/varTypes.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class varTypes {
+   public static void main(String[] args) {
+      
+      //Types of variables:
+      int number1 = 12;
+      long number2 = 50;
+      double number3 = 12.5d;
+      String variable1 = "Hello";
+      boolean variable2 = false;
+   System.out.println("Is the integer " + number1);
+   System.out.println("Is the long " + number2);
+   System.out.println("Is the double " + number3);
+   System.out.println("Is the String " + variable1);
+   System.out.println("Is the boolean " + variable2);
+   
+        
+    }
+}
